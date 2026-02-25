c11b819880c45d94a89708c021b9490ff54956d0
shulamitach <si0534170928+329@gmail.com>
2026-02-25 19:52:10 +0200

    Add comments to clarify sum calculation process
    Added explanation comments for the sum calculation logic.

--- Qu_4.cs
+++ Qu_4.cs
@@ -37,7 +37,15 @@ namespace task
                     timeOfSum[currentSum] = 1;
                 }
             }
-
+             /*explaintion:
+            we go on the whole array,
+            in each iteration' the current sum can be:
+            1.equal:we found more sub array that equal to x.
+            2.lower- than we continue..
+            3.bigger than we try to find in the hash table:
+            if we find that the difference between currentsum and x was also reached before.
+            that means we can drop those pieces of array and get exatally x,
+            the number of times that the diference was reached.*/
             return count;
         }
     }
